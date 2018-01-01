07d830e577951932e34221876e6b0fcbc5f96e35
xujintao <xujintao@126.com>
2018-01-01 10:18:07 +0800

    build.sh add libcommon

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
+make -C ./libcommon clean
 make -C ./libfcgi clean
 make -C ./libfastcgipp clean
 make -C ./app clean
 
+make -C ./libcommon
 make -C ./libfcgi 
 make -C ./libfastcgipp
 make -C ./app
