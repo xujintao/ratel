1cd0def6adcc74dd9dad39b29cf219ffb1dc8064
xujintao <xujintao@126.com>
2018-01-03 17:47:10 +0800

    pull out spawn-fcgi from app/main

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
+make -C ./libspawnfcgi clean
 make -C ./libcommon clean
 make -C ./libfcgi clean
 make -C ./libfastcgipp clean
 make -C ./app clean
 
+make -C ./libspawnfcgi
 make -C ./libcommon
 make -C ./libfcgi 
 make -C ./libfastcgipp
---
+++ libspawnfcgi/spawnfcgi.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    int spawn_fcgi(int argc, char **argv);
+
+#ifdef __cplusplus
+}
+#endif
